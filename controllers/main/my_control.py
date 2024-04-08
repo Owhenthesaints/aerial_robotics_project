0edcf22d9a5c2f041d94cf41f8bb3782cdf58a89
Owhenthesaints <owen.simon@epfl.ch>
2024-04-08 16:11:02 +0200

    put into place working basis for FSM

--- controllers/main/my_control.py
+++ controllers/main/my_control.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import cv2
+from enum import Enum
 
 # Global variables
 on_ground = True
@@ -10,17 +11,118 @@ height_desired = 1.0
 timer = None
 startpos = None
 timer_done = None
+THRESH_PINK = np.array([[140, 110, 110], [180, 255, 255]])
 
-# The available ground truth state measurements can be accessed by calling sensor_data[item]. All values of "item" are provided as defined in main.py lines 296-323. 
-# The "item" values that you can later use in the hardware project are:
+
+# All available ground truth measurements can be accessed by calling sensor_data[item], where "item" can take the following values:
 # "x_global": Global X position
 # "y_global": Global Y position
-# "range_down": Downward range finder distance (Used instead of Global Z distance)
-# "range_front": Front range finder distance
-# "range_left": Leftward range finder distance 
-# "range_right": Rightward range finder distance
-# "range_back": Backward range finder distance
+# "z_global": Global Z position
+# "roll": Roll angle (rad)
+# "pitch": Pitch angle (rad)
 # "yaw": Yaw angle (rad)
+# "v_x": Global X velocity
+# "v_y": Global Y velocity
+# "v_z": Global Z velocity
+# "v_forward": Forward velocity (body frame)
+# "v_left": Leftward velocity (body frame)
+# "v_down": Downward velocity (body frame)
+# "ax_global": Global X acceleration
+# "ay_global": Global Y acceleration
+# "az_global": Global Z acceleration
+# "range_front": Front range finder distance
+# "range_down": Donward range finder distance
+# "range_left": Leftward range finder distance 
+# "range_back": Backward range finder distance
+# "range_right": Rightward range finder distance
+# "range_down": Downward range finder distance
+# "rate_roll": Roll rate (rad/s)
+# "rate_pitch": Pitch rate (rad/s)
+# "rate_yaw": Yaw rate (rad/s)
+
+
+# class implementation of the FSM
+class StateEnum(Enum):
+    SEARCH_PINK = 0
+    GO_TO_PINK = 1
+    GO_TO_FZ = 2
+    FIND_LANDING_PAD = 3
+    BACK_FIND_PINK = 4
+    BACK_TO_PINK = 5
+    BACK_TO_START = 7  # do not search LP just go to LP stored in beginning
+
+
+DEFAULT_RESPONSE = (0, 0, height_desired, 0)
+
+
+def search_pink(sensor_data, camera_data):
+    case = StateEnum.SEARCH_PINK.value
+
+    hsv = cv2.cvtColor(camera_data, cv2.COLOR_BGR2HSV)
+
+    # Threshold the HSV image to get only pink colors
+    square_mask = cv2.inRange(hsv, THRESH_PINK[0], THRESH_PINK[1])
+
+    # if pink_not_found
+    if (not np.any(square_mask)):
+        return list(DEFAULT_RESPONSE), case
+
+    # Bitwise-AND mask and original image
+    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(square_mask)
+
+    largest_component_label = np.argmax(stats[1:, cv2.CC_STAT_AREA]) + 1
+
+    largest_component_mask = np.uint8(labels == largest_component_label) * 255
+
+    cv2.imshow('Camera Feed', largest_component_mask)
+    cv2.waitKey(1)
+    return list(DEFAULT_RESPONSE), case
+
+
+def go_to_pink(sensor_data, camera_data):
+    case = StateEnum.GO_TO_PINK.value
+    return list(DEFAULT_RESPONSE), case
+
+
+def go_to_fz(sensor_data, camera_data):
+    case = StateEnum.GO_TO_FZ
+    return list(DEFAULT_RESPONSE), case
+
+
+def find_landing_pad(sensor_data, camera_data):
+    case = StateEnum.FIND_LANDING_PAD
+    return list(DEFAULT_RESPONSE), case
+
+
+def back_find_pink(sensor_data, camera_data):
+    case = StateEnum.BACK_FIND_PINK
+    return list(DEFAULT_RESPONSE), case
+
+
+def back_to_pink(sensor_data, camera_data):
+    case = StateEnum.BACK_TO_PINK
+    return list(DEFAULT_RESPONSE), case
+
+
+def back_to_start(sensor_data, camera_data):
+    case = StateEnum.BACK_TO_START
+    return list(DEFAULT_RESPONSE), case
+
+
+def default_case(*args):
+    raise ValueError("state out of range")
+
+
+FSM_DICO = {
+    StateEnum.SEARCH_PINK.value: search_pink,
+    StateEnum.GO_TO_PINK.value: go_to_pink,
+    StateEnum.GO_TO_FZ.value: go_to_fz,
+    StateEnum.FIND_LANDING_PAD.value: find_landing_pad,
+    StateEnum.BACK_FIND_PINK.value: back_find_pink,
+    StateEnum.BACK_TO_PINK.value: back_to_pink,
+    StateEnum.BACK_TO_START.value: back_to_start
+}
+
 
 # This is the main function where you will implement your control algorithm
 def get_command(sensor_data, camera_data, dt):
@@ -31,64 +133,52 @@ def get_command(sensor_data, camera_data, dt):
     # cv2.imshow('Camera Feed', camera_data)
     # cv2.waitKey(1)
 
-    
     # Take off
     if startpos is None:
-        startpos = [sensor_data['x_global'], sensor_data['y_global'], sensor_data['range_down']]    
+        startpos = [sensor_data['x_global'], sensor_data['y_global'], sensor_data['z_global']]
     if on_ground and sensor_data['range_down'] < 0.49:
         control_command = [0.0, 0.0, height_desired, 0.0]
         return control_command
     else:
         on_ground = False
 
-
-
     # ---- YOUR CODE HERE ----
-    thresh_pink = np.array([[140, 110, 110], [180, 255, 255]])
+    # seting up the static_state value
+    def access_function(case):
+        nonlocal sensor_data, camera_data
+        return FSM_DICO.get(case, default_case)(sensor_data, camera_data)
 
-    control_command = [0.0, 0.0, height_desired, 0.0]
+    if not hasattr(get_command, "static_state"):
+        get_command.static_state = StateEnum.SEARCH_PINK.value
+
+    # activate the FSM
+    control_command, get_command.static_state = access_function(get_command.static_state)
+
     on_ground = False
-    hsv = cv2.cvtColor(camera_data, cv2.COLOR_BGR2HSV)
-
-
-    # Threshold the HSV image to get only pink colors
-    square_mask = cv2.inRange(hsv, thresh_pink[0], thresh_pink[1])
-
-    if(not np.any(square_mask)):
-        return control_command
-
-    # Bitwise-AND mask and original image
-    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(square_mask)
-
-    largest_component_label = np.argmax(stats[1:, cv2.CC_STAT_AREA]) + 1
-
-    largest_component_mask = np.uint8(labels == largest_component_label) * 255
-    
-    cv2.imshow('Camera Feed', square_mask)
-    cv2.waitKey(1)
     # map = occupancy_map(sensor_data)
-    
-    return control_command # Ordered as array with: [v_forward_cmd, v_left_cmd, alt_cmd, yaw_rate_cmd]
+
+    return control_command  # [vx, vy, alt, yaw_rate]
 
 
 # Occupancy map based on distance sensor
-min_x, max_x = 0, 5.0 # meter
-min_y, max_y = 0, 5.0 # meter
-range_max = 2.0 # meter, maximum range of distance sensor
-res_pos = 0.2 # meter
-conf = 0.2 # certainty given by each measurement
-t = 0 # only for plotting
+min_x, max_x = 0, 5.0  # meter
+min_y, max_y = 0, 5.0  # meter
+range_max = 2.0  # meter, maximum range of distance sensor
+res_pos = 0.2  # meter
+conf = 0.2  # certainty given by each measurement
+t = 0  # only for plotting
 
-map = np.zeros((int((max_x-min_x)/res_pos), int((max_y-min_y)/res_pos))) # 0 = unknown, 1 = free, -1 = occupied
+map = np.zeros((int((max_x - min_x) / res_pos), int((max_y - min_y) / res_pos)))  # 0 = unknown, 1 = free, -1 = occupied
+
 
 def occupancy_map(sensor_data):
     global map, t
     pos_x = sensor_data['x_global']
     pos_y = sensor_data['y_global']
     yaw = sensor_data['yaw']
-    
-    for j in range(4): # 4 sensors
-        yaw_sensor = yaw + j*np.pi/2 #yaw positive is counter clockwise
+
+    for j in range(4):  # 4 sensors
+        yaw_sensor = yaw + j * np.pi / 2  # yaw positive is counter clockwise
         if j == 0:
             measurement = sensor_data['range_front']
         elif j == 1:
@@ -97,11 +187,11 @@ def occupancy_map(sensor_data):
             measurement = sensor_data['range_back']
         elif j == 3:
             measurement = sensor_data['range_right']
-        
-        for i in range(int(range_max/res_pos)): # range is 2 meters
-            dist = i*res_pos
-            idx_x = int(np.round((pos_x - min_x + dist*np.cos(yaw_sensor))/res_pos,0))
-            idx_y = int(np.round((pos_y - min_y + dist*np.sin(yaw_sensor))/res_pos,0))
+
+        for i in range(int(range_max / res_pos)):  # range is 2 meters
+            dist = i * res_pos
+            idx_x = int(np.round((pos_x - min_x + dist * np.cos(yaw_sensor)) / res_pos, 0))
+            idx_y = int(np.round((pos_y - min_y + dist * np.sin(yaw_sensor)) / res_pos, 0))
 
             # make sure the current_setpoint is within the map
             if idx_x < 0 or idx_x >= map.shape[0] or idx_y < 0 or idx_y >= map.shape[1] or dist > range_max:
@@ -113,28 +203,31 @@ def occupancy_map(sensor_data):
             else:
                 map[idx_x, idx_y] -= conf
                 break
-    
-    map = np.clip(map, -1, 1) # certainty can never be more than 100%
+
+    map = np.clip(map, -1, 1)  # certainty can never be more than 100%
 
     # only plot every Nth time step (comment out if not needed)
     if t % 50 == 0:
-        plt.imshow(np.flip(map,1), vmin=-1, vmax=1, cmap='gray', origin='lower') # flip the map to match the coordinate system
+        plt.imshow(np.flip(map, 1), vmin=-1, vmax=1, cmap='gray',
+                   origin='lower')  # flip the map to match the coordinate system
         plt.savefig("map.png")
         plt.close()
-    t +=1
+    t += 1
 
     return map
 
 
 # Control from the exercises
 index_current_setpoint = 0
-def path_to_setpoint(path,sensor_data,dt):
+
+
+def path_to_setpoint(path, sensor_data, dt):
     global on_ground, height_desired, index_current_setpoint, timer, timer_done, startpos
 
     # Take off
     if startpos is None:
-        startpos = [sensor_data['x_global'], sensor_data['y_global'], sensor_data['range_down']]    
-    if on_ground and sensor_data['range_down'] < 0.49:
+        startpos = [sensor_data['x_global'], sensor_data['y_global'], sensor_data['z_global']]
+    if on_ground and sensor_data['z_global'] < 0.49:
         current_setpoint = [startpos[0], startpos[1], height_desired, 0.0]
         return current_setpoint
     else:
@@ -149,17 +242,20 @@ def path_to_setpoint(path,sensor_data,dt):
     # Hover at the final setpoint
     if index_current_setpoint == len(path):
         # Uncomment for KF
-        control_command = [startpos[0], startpos[1], startpos[2]-0.05, 0.0]
+        control_command = [startpos[0], startpos[1], startpos[2] - 0.05, 0.0]
 
         if timer_done is None:
             timer_done = True
-            print("Path planing took " + str(np.round(timer,1)) + " [s]")
+            print("Path planing took " + str(np.round(timer, 1)) + " [s]")
         return control_command
 
     # Get the goal position and drone position
     current_setpoint = path[index_current_setpoint]
-    x_drone, y_drone, z_drone, yaw_drone = sensor_data['x_global'], sensor_data['y_global'], sensor_data['range_down'], sensor_data['yaw']
-    distance_drone_to_goal = np.linalg.norm([current_setpoint[0] - x_drone, current_setpoint[1] - y_drone, current_setpoint[2] - z_drone, clip_angle(current_setpoint[3]) - clip_angle(yaw_drone)])
+    x_drone, y_drone, z_drone, yaw_drone = sensor_data['x_global'], sensor_data['y_global'], sensor_data['z_global'], \
+    sensor_data['yaw']
+    distance_drone_to_goal = np.linalg.norm(
+        [current_setpoint[0] - x_drone, current_setpoint[1] - y_drone, current_setpoint[2] - z_drone,
+         clip_angle(current_setpoint[3]) - clip_angle(yaw_drone)])
 
     # When the drone reaches the goal setpoint, e.g., distance < 0.1m
     if distance_drone_to_goal < 0.1:
@@ -172,10 +268,11 @@ def path_to_setpoint(path,sensor_data,dt):
 
     return current_setpoint
 
+
 def clip_angle(angle):
-    angle = angle%(2*np.pi)
+    angle = angle % (2 * np.pi)
     if angle > np.pi:
-        angle -= 2*np.pi
+        angle -= 2 * np.pi
     if angle < -np.pi:
-        angle += 2*np.pi
+        angle += 2 * np.pi
     return angle
